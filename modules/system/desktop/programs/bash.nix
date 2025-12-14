44b2a0d052294f950ce7f39509efd98885f5617a
Destructor-Ben <ben.honeyfield@gmail.com>
2025-12-15 01:21:33 +1300

    Remove programs.bash.enable

--- modules/system/desktop/programs/bash.nix
+++ modules/system/desktop/programs/bash.nix
@@ -1,6 +1,4 @@
 {
-  programs.bash.enable = true;
-
   # Symlink bash into /bin/bash so external programs don't cry about it
   systemd.tmpfiles.rules = [
     "L+ /bin/bash - - - - /run/current-system/sw/bin/bash"
