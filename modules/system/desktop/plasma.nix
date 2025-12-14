336ad3a2f0a7b4b1c19fca841e94df10590607fe
Destructor-Ben <ben.honeyfield@gmail.com>
2025-12-15 01:26:29 +1300

    Add SDDM theme

--- modules/system/desktop/plasma.nix
+++ modules/system/desktop/plasma.nix
@@ -1,5 +1,14 @@
+{ pkgs, ... }:
 {
-  services.displayManager.sddm.enable = true;
+  environment.systemPackages = with pkgs; [ sddm-astronaut ];
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "sddm-astronaut";
+    extraPackages = [ pkgs.sddm-astronaut ];
+  };
+
   services.desktopManager.plasma6.enable = true;
 
   # X11 required for SDDM
