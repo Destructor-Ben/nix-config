8e14572e4d8e6737054b51d3c196cfd01218626d
Destructor-Ben <ben.honeyfield@gmail.com>
2025-12-16 16:07:56 +1300

    Fix zsh config

--- modules/user/programs/zsh.nix
+++ modules/user/programs/zsh.nix
@@ -10,5 +10,20 @@
       ns = "sudo nixos-rebuild switch --flake ~/nix-config";
       ngc = "ns && sudo nix-collect-garbage -d && ns";
     };
+
+    initContent = ''
+      # Powerlevel10k theme
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
+      # Load your p10k config (if it exists)
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    '';
   };
+
+  home.file.".p10k.zsh".source = ../dotfiles/.p10k.zsh;
+
+  home.packages = with pkgs; [
+    zsh-powerlevel10k
+    meslo-lgs-nf
+  ];
 }
