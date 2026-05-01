2b0d60f65b1f3b287c6d3c9d7cff3dd0d09d65d3
Destructor-Ben <destructorben208@gmail.com>
2026-05-01 21:28:09 +1200

    fix hardcode in wlogout script

--- modules/user/desktop/scripts.nix
+++ modules/user/desktop/scripts.nix
@@ -1,14 +1,18 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, theme, ... }:
 let
   fs = lib.fileset;
   scriptPaths = fs.toList (fs.fileFilter (file: lib.hasSuffix ".sh" file.name) ../../../scripts);
   scriptPkgs = builtins.listToAttrs (map (path: 
     let
       name = lib.removeSuffix ".sh" (baseNameOf path);
+      rawContents = builtins.readFile path;
+      finalContents = if name == "wlogout-custom"
+        then lib.replaceStrings [ "@PADDING@" "@SCREEN_WIDTH@" "@SCREEN_HEIGHT@" ] [ (toString theme.padding) (toString 1920) (toString 1080) ] rawContents
+        else rawContents;
     in
     {
       inherit name;
-      value = pkgs.writeShellScriptBin name (builtins.readFile path);
+      value = pkgs.writeShellScriptBin name finalContents;
     }
   ) scriptPaths);
 in
--- scripts/wlogout-custom.sh
+++ scripts/wlogout-custom.sh
@@ -1,5 +1,5 @@
 button_size=100
-padding=10 # Hardcode, idc
-padding_h=$(( (1920 - $button_size * 5 - $padding * 4) / 2 ))
-padding_v=$(( (1080 - $button_size) / 2 ))
+padding=@PADDING@
+padding_h=$(( (@SCREEN_WIDTH@ - $button_size * 5 - $padding * 4) / 2 ))
+padding_v=$(( (@SCREEN_HEIGHT@ - $button_size) / 2 ))
 wlogout -b 5 -L $padding_h -R $padding_h -T $padding_v -B $padding_v -c $padding -r $padding
